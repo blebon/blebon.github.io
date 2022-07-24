4cdd75dbc8459932a7924e9e627263081cbfb327
Bruno <32cct7rk@anonaddy.me>
2022-07-24 16:24:18 +0100

    Let workbox handle precache list

--- sw.js
+++ sw.js
@@ -1,22 +1,33 @@
 ---
 layout: none
 ---
-importScripts("https://storage.googleapis.com/workbox-cdn/releases/6.5.3/workbox-sw.js");
+importScripts('https://storage.googleapis.com/workbox-cdn/releases/6.5.3/workbox-sw.js');
 
 const { registerRoute } = workbox.routing;
 const { CacheFirst, NetworkFirst, StaleWhileRevalidate } = workbox.strategies;
 const { CacheableResponse } = workbox.cacheableResponse;
 
 workbox.core.setCacheNameDetails({
-  prefix: "blebon.com",
-  suffix: '{{ site.time | date: "%Y-%m" }}'
+  prefix: 'blebon.com',
+  suffix: '{{ site.time | date: "%Y-%m" }}',
+  precache: 'precache',
+  runtime: 'runtime-cache'
 });
 
+// let Service Worker take control of pages ASAP
+workbox.core.skipWaiting();
+workbox.core.clientsClaim();
+
+// let Workbox handle our precache list
+workbox.precaching.precacheAndRoute(self.__precacheManifest);
+
+// use `NetworkFirst`strategy for home page
 registerRoute(
-  "/",
+  '/',
   new NetworkFirst()
 );
 
+// Pages
 registerRoute(
   /about/,
   new NetworkFirst()
@@ -27,22 +38,14 @@ registerRoute(
   new NetworkFirst()
 );
 
+// Posts
 registerRoute(
-  new RegExp("/\\d{4}/\\d{2}/\\d{2}/.+"),
+  new RegExp('/\\d{4}/\\d{2}/\\d{2}/.+'),
   new StaleWhileRevalidate()
 );
 
+// css, images
 registerRoute(
-  /assets/css/,
+  /assets/(css|img|icons)/,
   new CacheFirst()
 );
-
-workbox.precaching.precacheAndRoute([
-  {%- for post in site.posts limit:15 %}
-    { url: '{{ post.url }}', revision: '{{ post.date | date: "%Y-%m-%d"}}' },
-  {%- endfor %}
-  { url: '/', revision: '{{ site.time | date: "%Y%m%d%H%M" }}' },
-  { url: '/about/', revision: '{{ site.time | date: "%Y%m%d%H%M" }}' },
-  { url: '/mauritian-blogs/', revision: '{{ site.time | date: "%Y%m%d%H%M" }}' },
-  { url: '/assets/css/style.css', revision: '{{ site.time | date: "%Y%m%d%H%M" }}' }
-]);
